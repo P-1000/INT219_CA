5e915eab59b176a7e9a1303bee19f2d8f6c26eb9
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-10-15 16:04:29 +0530

    new changes chesa bro with routing and nad new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,30 @@ import { HeroComponent } from './component/hero/hero.component';
 import { ServicesComponent } from './component/services/services.component';
 import { TestimonialsComponent } from './component/testimonials/testimonials.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { AuthComponent } from './component/auth/auth.component';
+// import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {Routes , RouterModule} from '@angular/router';
+import { HomelandComponent } from './component/homeland/homeland.component';
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDkR4MESgjh20z9yaM1s1AXX99qquDn2ss",
+//   authDomain: "sanfoundry-sc.firebaseapp.com",
+//   projectId: "sanfoundry-sc",
+//   storageBucket: "sanfoundry-sc.appspot.com",
+//   messagingSenderId: "527997111994",
+//   appId: "1:527997111994:web:35a22b150a14f2e3c1bc83",
+//   measurementId: "G-8Q04XQCCSR"
+// };
+
+
+
+const routesConfig: Routes = [
+    { path: '', component: HomelandComponent },
+    { path: 'login', component: AuthComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,12 +44,16 @@ import { ContactComponent } from './component/contact/contact.component';
     ServicesComponent,
     TestimonialsComponent,
     ContactComponent,
+    AuthComponent,
+    HomelandComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routesConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
